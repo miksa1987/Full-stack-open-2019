eb1c0a9022abcae95342cb3f3318a75118686370
x <mika87@gmail.com>
2019-04-27 08:53:26 +0300

    1.2 tehty

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -5,6 +5,10 @@ const Header = ( { course } ) => {
     return (<div><h1>{course}</h1></div>)
 }
 
+const Part = ( { name, exercises }) => {
+  return (<div><p>{name} {exercises}</p></div>)
+}
+
 const Content = () => {
   const part1 = 'Reactin perusteet'
   const exercises1 = 10
@@ -14,15 +18,9 @@ const Content = () => {
   const exercises3 = 14
 
   return (<div>
-    <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+    <Part name={part1} exercises={exercises1} />
+    <Part name={part2} exercises={exercises2} />
+    <Part name={part3} exercises={exercises3} />
   </div>)
 }
 
@@ -32,7 +30,7 @@ const Total = ( { total } ) => {
 
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
-  
+
   return (
     <div>
       <Header course={course} />
