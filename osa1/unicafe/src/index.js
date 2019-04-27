16d5c4761fe511026eb69c654fb04a279cacedc1
x <mika87@gmail.com>
2019-04-27 14:17:20 +0300

    1.7 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -6,17 +6,39 @@ const App = () => {
   const [good, setGood]         = useState(0)
   const [neutral, setNeutral]   = useState(0)
   const [bad, setBad]           = useState(0)
+  const [total, setTotal]       = useState(0)
+  const [points, setPoints]     = useState(0)
+
+  const clickGood = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+    setPoints(points + 1)
+  }
+
+  const clickNeutral = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+
+  const clickBad = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+    setPoints(points - 1)
+  }
 
   return (
     <div>
       <h1>Anna palautetta</h1>
-      <button onClick={() => setGood(good + 1)} >Hyvä</button>
-      <button onClick={() => setNeutral(neutral + 1)} >Neutraali</button>
-      <button onClick={() => setBad(bad + 1)} >Huono</button>
+      <button onClick={clickGood} >Hyvä</button>
+      <button onClick={clickNeutral} >Neutraali</button>
+      <button onClick={clickBad} >Huono</button>
       <h1>Statistiikka</h1>
       <p>hyvä {good}</p>
       <p>neutraali {neutral}</p>
       <p>huono {bad}</p>
+      <p>yhteensä {total}</p>
+      <p>keskiarvo {total === 0 ? 0 : points / total}</p>
+      <p>positiivisia {total === 0 ? 0 : good / total * 100} %</p>
     </div>
   )
 }
