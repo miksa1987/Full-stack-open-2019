c7365bf12b96e7fab6fa2326362f8c370490cc78
x <mika87@gmail.com>
2019-05-09 15:54:19 +0300

    4.5 tehty

--- osa4/blogilista/util/list_helper.js
+++ osa4/blogilista/util/list_helper.js
@@ -2,6 +2,33 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let total = 0
+  blogs.forEach(blog => {
+    total += blog.likes
+  })
+
+  return total
+}
+
+const favoriteBlog = (blogs) => {
+  let favorite = {
+    title: '',
+    author: '',
+    likes: 0
+  }
+
+  blogs.forEach(blog => {
+    if(blog.likes > favorite.likes) {
+      favorite.title = blog.title
+      favorite.author = blog.author
+      favorite.likes = blog.likes
+    }
+  })
+
+  return favorite
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes, favoriteBlog
 }
