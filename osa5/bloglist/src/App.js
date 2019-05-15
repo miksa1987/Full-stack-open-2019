e5ca4e3924d807e0ae8896ef37fef06774165021
x <mika87@gmail.com>
2019-05-15 20:21:23 +0300

    5.2 tehty

--- osa5/bloglist/src/App.js
+++ osa5/bloglist/src/App.js
@@ -11,18 +11,29 @@ const App = () => {
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    ) 
+    if(window.localStorage.getItem('user') !== null) {
+      setUser(JSON.parse(window.localStorage.getItem('user')))
+    }
   }, [])
 
   useEffect(() => {
-    
+    if(user !== null && user.name && user.username && user.token) {
+      console.log('LOGGED!')
+      window.localStorage.setItem('user', JSON.stringify(user))
+    }
   }, [user])
 
+  const logout = () => {
+    setUser(null)
+    window.localStorage.clear()
+  }
   if(user) {
     return (
       <div>
         <h2>blogs</h2>
         <p>{user.name} logged in</p>
+        <button onClick={logout}>Log out</button>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
