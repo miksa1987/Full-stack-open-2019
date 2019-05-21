9e7eff676243614819f0e98e92c7be591347ae35
x <mika87@gmail.com>
2019-05-21 19:50:24 +0300

    5.14 tehty

--- osa5/bloglist/src/components/SimpleBlog.test.js
+++ osa5/bloglist/src/components/SimpleBlog.test.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import 'jest-dom/extend-expect'
-import {render, cleanup} from 'react-testing-library'
+import {render, cleanup, fireEvent} from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
+
+const blog = {
+  author: 'Mika Laaksonen',
+  title: 'Reactin testauksen taide',
+  likes: 5
+}
 
 afterEach(cleanup)
 
 test('renders content correctly', () => {
-  const blog = {
-    author: 'Mika Laaksonen',
-    title: 'Reactin testauksen taide',
-    likes: 5
-  }
-
   const component = render(
     <SimpleBlog blog={blog} />
   )
@@ -24,4 +24,18 @@ test('renders content correctly', () => {
   expect(Likes).toHaveTextContent('blog has 5 likes')
 
   component.debug()
+})
+
+test('Like button works as supposed', () => {
+  const mockClicker = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockClicker} />
+  )
+  
+  const likeButton = getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(mockClicker.mock.calls.length).toBe(2)
 })
