33fad9faefd9450e99befd6097a7465e50f7b52d
x <mika87@gmail.com>
2019-05-31 12:44:11 +0300

    7.2 done!

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -13,12 +13,20 @@ const Menu = () => {
     </div>
   )
 }
+const Anecdote = ({ anecdote }) => {
+  return ( <div>
+    <h3>{anecdote.content} by {anecdote.author}</h3>
+    <p>has {anecdote.votes} votes</p>
+    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+  </div>)
+}
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} >
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link></li>)}
     </ul>
   </div>
 )
@@ -132,10 +140,11 @@ const App = () => {
         <Route exact path='/' render={ () => <AnecdoteList anecdotes={anecdotes} /> } />
         <Route path='/create' render={ () => <CreateNew addNew={addNew} /> } />
         <Route path='/about' render={ () => <About /> } />
+        <Route exact path='/anecdotes/:id' render={({ match }) => <Anecdote anecdote={anecdoteById(match.params.id)} /> } />
       </Router>
       <Footer />
     </div>
   )
 }
 
-export default App;
+export default App
