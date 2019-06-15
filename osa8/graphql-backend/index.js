410633c22ddb9104a585bed82b53e99d8b279e00
miksa1987 <mika87@gmail.com>
2019-06-15 09:44:37 +0300

    8.2 tehty

--- osa8/graphql-backend/index.js
+++ osa8/graphql-backend/index.js
@@ -84,16 +84,32 @@ const books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount:    () => books.length,
-    authorCount:  () => authors.length
+    authorCount:  () => authors.length,
+    allBooks:     () => books
   }
 }
 
