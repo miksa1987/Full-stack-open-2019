ad3cd8f9dc9462bdbefbb9a3d751cce53da5dbbd
miksa1987 <mika87@gmail.com>
2019-06-17 09:53:31 +0300

    8.8 & 8.9 tehty

---
+++ osa8/library-front/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider} from 'react-apollo-hooks'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <ApolloHooksProvider client={client}>
+      <App />
+    </ApolloHooksProvider>
+  </ApolloProvider>
+  , document.getElementById('root'))
